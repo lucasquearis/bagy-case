2425c2d0152d04ee04221dfc000aeea213e4ef2f
lucasquearis <lucasquearis@hotmail.com>
2022-01-11 10:55:33 -0300

    instala e configura sqlite no knex

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/bagy.db3'
+  },
+  useNullAsDefault: true,
+  migrations: {
+    tableName: "knex_migrations",
+    directory: './src/db/migrations'
+  },
+  seeds: {
+    directory: './data/seeds'
+  }
+};
