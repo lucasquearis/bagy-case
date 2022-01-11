154e55228f7714aeb603d88ee6d87983bc167f15
lucasquearis <lucasquearis@hotmail.com>
2022-01-11 11:01:24 -0300

    instala sqlite3

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,18 @@
 // Update with your config settings.
 
 module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: './data/bagy.db3'
-  },
-  useNullAsDefault: true,
-  migrations: {
-    tableName: "knex_migrations",
-    directory: './src/db/migrations'
-  },
-  seeds: {
-    directory: './data/seeds'
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/bagy.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: './src/db/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
