92484449af64a6c39913923d53f8d52f3484e514
lucasquearis <lucasquearis@hotmail.com>
2022-01-12 12:11:57 -0300

    cria seed clientes e adiciona comandos package.json

---
+++ data/seeds/01_clientes.js
@@ -0,0 +1,34 @@
+// https://www.4devs.com.br/gerador_de_pessoas
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('clientes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('clientes').insert([
+        {
+          nomeCompleto: 'Mario da Silva',
+          email: 'mariosilva@email.com',
+          cpf: '040.234.180-56',
+          dataNascimento: '16/08/1987'
+        },
+        {
+          nomeCompleto: 'Marina Alves da Costa',
+          email: 'marina.dev@email.com',
+          cpf: '554.296.060-06',
+          dataNascimento: '7/10/1995'
+        },
+        {
+          nomeCompleto: 'Rodrigo Bruno Souza',
+          email: 'rrodrigobrunosouza@projetemovelaria.com.br',
+          cpf: '619.906.789-47',
+          dataNascimento: '02/01/2003'
+        },
+        {
+          nomeCompleto: 'Augusto Lucca Rafael da Costa',
+          email: 'augustoluccarafaeldacosta_@nhrtaxiaereo.com',
+          cpf: '253.039.189-04',
+          dataNascimento: '10/01/2003'
+        },
+      ]);
+    });
+};
