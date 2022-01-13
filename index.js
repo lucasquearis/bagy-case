5faa38a89feaaed8fdd2bbcb94b07f380820ca59
lucasquearis <lucasquearis@hotmail.com>
2022-01-13 08:33:06 -0300

    instala jest e altera erros lint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { ApolloServer } = require('apollo-server');
-const { resolvers, typeDefs } = require('./src/graphql')
+const { resolvers, typeDefs } = require('./src/graphql');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   formatError: (err) => {
-    if(err.message) {
-      return err.message
+    if (err.message) {
+      return err.message;
     }
-  }
+  },
 });
 
 server.listen().then(({ url }) => console.log(`APP ONLINE NESSE LINK: ${url}`));
--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: './data/bagy.db'
+    filename: './data/bagy.db',
   },
   useNullAsDefault: true,
   migrations: {
-    tableName: "knex_migrations",
-    directory: './src/db/migrations'
+    tableName: 'knex_migrations',
+    directory: './src/db/migrations',
   },
   seeds: {
-    directory: './data/seeds'
-  }
+    directory: './data/seeds',
+  },
 };
